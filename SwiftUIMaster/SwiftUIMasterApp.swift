a76d033c7cf032faf60dbf74751cf289ec7a22ab
ranjin <jin7618@qq.com>
2023-06-07 09:37:50 +0800

    first commit

---
+++ SwiftUIMaster/SwiftUIMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMasterApp.swift
+//  SwiftUIMaster
+//
+//  Created by apus on 2023/6/7.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
